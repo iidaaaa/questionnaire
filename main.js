c455d84f279121696f002e5c1a45c54a681b25b5
iidaaaa <iida.oshima@gmail.com>
2021-12-17 13:28:10 +0900

    1217

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 $(function(){
-    for (var i = 1; i <= 4; i++){
-        $('#daimei').append(i);
-        $('#loop-list').append('<h1>'+i+'</h1><figure><img class="lazy" src="media/'+i+'.jpg" width="300" height="300" alt="'+i+'" /><audio controls src="media/'+i+'.wav"/></figure><hr>');
-    }
+    var array = ['01M', "02M", "03F", "04M", "05F", "07F", "08F", "09F", "10M"]
+    var count = 0
+    $.each(array, function(){
+        count += 1
+        var randomNum = Math.floor(Math.random()*((5+1)-1)+1);
+        $('#daimei').append(count);
+        $('#loop-list').append('<h1>'+count+'</h1><figure><img class="lazy" src="media/'+this+'/'+randomNum+'.jpg" width="300" height="300" alt='+randomNum+' "/><audio controls src="media/'+this+'/'+randomNum+'.wav"/></figure><hr>');
+    })
+
 });
 
 
